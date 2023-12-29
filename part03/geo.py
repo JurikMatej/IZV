067a85651a6d65d03fec6f2e7f323ab382aa5ebf
Matej Jurík <matej.jurik@nexigroup.com>
2023-12-29 16:33:14 +0100

    Implement task 1 in geo.py & add project resources

--- part03/geo.py
+++ part03/geo.py
@@ -3,25 +3,66 @@
 import pandas as pd
 import geopandas
 import matplotlib.pyplot as plt
-import contextily
+import contextily as ctx
 import sklearn.cluster
 import numpy as np
-# muzete pridat vlastni knihovny
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
-    pass
+
+    df = df.dropna(subset=['d', 'e'])
+    df.reset_index()
+    return geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df['d'], df['e']), crs='epsg:5514')
 
 def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
     """ Vykresleni grafu s nehodami  """
-    pass
+
+    region = "JHM"
+    years_to_plot = [2021, 2022]
+    # basemap_provider = ctx.providers.Stamen.Terrain
+    basemap_provider = ctx.providers.OpenStreetMap.Mapnik
+
+    data = gdf.copy()
+    data = data[data['region'] == region]
+
+    data = data[data['p10'] == 4]
+    data = data[data['date'].dt.year.isin(years_to_plot)]
+
+    data.to_crs("epsg:3857")
+
+    fig, axs = plt.subplots(1, 2, figsize=(16, 8))
+
+    # source=ctx.providers.Stamen.TonerLite
+
+    for ax, year in zip(axs, [2021, 2022]):
+        gdf[gdf['date'].dt.year == year].plot(ax=ax)
+        print("Downloading the basemap")
+        ctx.add_basemap(ax, crs=data.crs.to_string(), source=basemap_provider, zoom=15)  # TODO hangs
+        print("Hotovinko sak")
+
+    if show_figure:
+        plt.show()
+
+    if fig_location:
+        plt.savefig(fig_location)
+
+    plt.close()
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
     """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
-    pass
+
+    # TODO clustering
+
+    if show_figure:
+        plt.show()
+
+    if fig_location:
+        plt.savefig(fig_location)
+
+    plt.close()
 
 if __name__ == "__main__":
     # zde muzete delat libovolne modifikace
