a0f0c7ef6edee204616c8fde23ff37512c33f057
Matej <jurik.matejw@gmail.com>
2024-01-01 16:38:29 +0100

    Add docstrings where necessary

--- part03/geo.py
+++ part03/geo.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3.10
 # coding=utf-8
+"""
+IZV project part 3
+Author: xjurik12
+Python version: 3.12
+"""
 import pandas as pd
 import geopandas
 import matplotlib.pyplot as plt
@@ -15,12 +20,18 @@ EPSG_5514_CZECHIA_BOUNDARIES = (-911041.21, -1244158.89, -418597.43, -935719.38)
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
-    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani """
+    """
+    Convert the source dataframe to geopandas.GeoDataFrame with the right coordinate system applied
 
+    :param df: source
+    :return: geopandas.GeoDataFrame with the contents of the source df filtered to relevant columns
+             to the assignment
+    """
+
+    # Keep only relevant data
     df = df.dropna(subset=['d', 'e'])
     df.reset_index()
 
-    # Keep only relevant data
     df = df[['p1', 'p10', 'p11', 'date', 'region', 'd', 'e']]
 
     # Create a GeoDataFrame with the Krovak projection
@@ -34,12 +45,16 @@ def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     return gdf.to_crs('EPSG:3857')  # Convert to Web Mercator projection to fix potential graph distortion
 
 
-def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
-             show_figure: bool = False):
-    """ Vykresleni grafu s nehodami  """
+def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None, show_figure: bool = False):
+    """
+    Plot a graph with geographical marks of accidents that were caused by an animal in pre-selected region
+    over years 2021 and 2022
 
+    :param gdf: geopandas.GeoDataFrame provided by make_geo() function
+    :param fig_location: path to save the result graph to
+    :param show_figure: display the result graph if True
+    """
     years_to_plot = [2021, 2022]
-    plot_color_by_year = ["tab:red", "tab:red"]
 
     data = gdf.copy()
     data = data[data['region'] == REGION]
@@ -50,13 +65,13 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     fig, axs = plt.subplots(1, 2, figsize=(12, 8))
 
     # Plot maps with the accidents marked as colored dots
-    for ax, year, color in zip(axs, years_to_plot, plot_color_by_year):
-        data[data['date'].dt.year == year].envelope.plot(ax=ax, markersize=4, color=color)
+    for ax, year in zip(axs, years_to_plot):
+        data[data['date'].dt.year == year].plot(ax=ax, markersize=4, color="tab:red")
         ctx.add_basemap(ax, crs=data.crs.to_string(), source=BASEMAP_PROVIDER, alpha=0.9)
 
         ax.set_title(f"{REGION} kraj ({year})")
 
-    # Finally, configure axis for both plots
+    # Finally, configure axis for both plots (so they share the same size)
     (xmin1, xmax1), (ymin1, ymax1) = axs[0].get_xlim(), axs[0].get_ylim()
     (xmin2, xmax2), (ymin2, ymax2) = axs[1].get_xlim(), axs[1].get_ylim()
 
@@ -79,10 +94,17 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     plt.close()
 
 
-def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
-                 show_figure: bool = False):
-    """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
+def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None, show_figure: bool = False):
+    """
+    Plot a graph showing the accidents caused while the driver was under the influence of alcohol.
+    Data is used from all the years the data is available from, but limited to a pre-selected region.
+    Furthermore, the plots in the graph are clustered by their respective location to form groups of
+    accidents in close proximity.
 
+    :param gdf: geopandas.GeoDataFrame provided by make_geo() function
+    :param fig_location: path to save the result graph to
+    :param show_figure: display the result graph if True
+    """
     data = gdf.copy()
     data = data[
         data['region'] == REGION  # NOTE: Not filtering out irrelevant regions in make_geo is a part of the assignment
@@ -102,6 +124,12 @@ def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     #                       that seem too remote to be picked up)
     # - KMeans          -> Personal preference of all the results.
     #                      Clusters are generally well-placed over close cities in close proximity
+    #
+    # The parameters (n_clusters=15 & n_init=14) make for the most visually pleasing result I found.
+    # More clusters result in clusters that are too small and lessening the number of clusters leads to areas too large
+    #
+    # In my opinion, n_init=14 hits the sweet spot for the clusters to contain just the right accidents
+    # based on proximity to their cities
     model = cluster.KMeans(n_clusters=15, n_init=14)
     model = model.fit(data_coords)
 
@@ -119,7 +147,7 @@ def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                   "pad": 0.014,
                   "label": "Počet nehod v úseku"})
 
-    ctx.add_basemap(ax, crs=data.crs.to_string(), alpha=0.9, source=BASEMAP_PROVIDER)  # Basemap
+    ctx.add_basemap(ax, crs=data.crs.to_string(), alpha=0.9, source=BASEMAP_PROVIDER)  # Add basemap
 
     ax.set_title(f"Nehody v {REGION} kraji s výynamnou měrou alkoholu")
     ax.set_axis_off()
@@ -136,7 +164,6 @@ def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
 
 
 if __name__ == "__main__":
-    # zde muzete delat libovolne modifikace
     gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
     plot_geo(gdf, "geo1.png", True)
     plot_cluster(gdf, "geo2.png", True)
